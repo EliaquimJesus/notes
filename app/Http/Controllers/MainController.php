3af7b9f915350564a7dc0bdfebc204f33794f274
Eliaquim da Luz <eliaquim.daluz@yahoo.com>
2024-09-30 14:04:57 +0100

    027

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -22,5 +24,35 @@ class MainController extends Controller
     public function newNote(): string
     {
         return 'APP NEW NOTE CREATE!';
+    }
+
+    public function editNote($id): string
+    {
+        
+        $id = $this->decryptId($id);
+        
+        return "I´m editing note with id = $id";
+    }
+
+    public function deleteNote($id): string
+    {
+        $id = $this->decryptId($id);
+        
+        return "I´m deleting note with id = $id";
+    }
+
+    private function decryptId($id): mixed
+    {
+        // check if id is encrypyed
+        try {
+            //code...
+            $id = Crypt::decrypt($id);
+
+        } catch (DecryptException $e) {
+            //throw $th;
+            return redirect()->route('home');
+        }
+
+        return $id;
     }
 }
