ed56f9d3d745085e0999f5282c76eee23fef4b55
Eliaquim da Luz <eliaquim.daluz@yahoo.com>
2024-10-01 16:49:02 +0100

    030

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,11 +12,8 @@ use Illuminate\Contracts\View\View;
 
 class MainController extends Controller
 {
-    public function __construct(private User $user, private Note $note)
-    {
-        
-    }
-    
+    public function __construct(private User $user, private Note $note) {}
+
     public function index(): View
     {
         // load user´s notes
@@ -69,20 +66,62 @@ class MainController extends Controller
     }
 
     // edit a note
-    public function editNote(string $id): string 
+    public function editNote(string $id): View
     {
+        // get the id
         $id = Operations::decryptId($id);
-        
-        return "I´m editing note with id = $id";
+
+        // load note
+        $note = $this->note::find($id);
+
+        // return the edit note view
+        return view('edit_note', ['note' => $note]);
+    }
+
+    public function editNoteSubmit(Request $request)
+    {
+        // validate request
+        $request->validate(
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note'  => 'required|min:3|max:3000'
+            ],
+            [
+                'text_title.required'     => 'O titulo é obrigatório',
+                'text_title.min' => 'O titulo deve ter pelo menos :min caracteres',
+                'text_title.max' => 'O titulo deve ter no máximo :max caracteres',
+
+                'text_note.required'      => 'A nota é obrigatória',
+                'text_note.min'  => 'A nota deve ter pelo menos :min caracteres',
+                'text_note.max'  => 'A nota deve ter no máximo :max caracteres',
+            ]
+        );
+
+        // check if note id exists
+        if ($request->note_id === null) {
+            return redirect()->route('home');
+        }
+
+        // decrypt note_id
+        $id = Operations::decryptId($request->note_id);
+
+        // load note
+        $note = $this->note::find($id);
+
+        // update note
+        $note->title = $request->get('text_title');
+        $note->text  = $request->get('text_note');
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home')->with('success', 'updated successfully');;
     }
 
     // delete a note
-    public function deleteNote(string $id): string 
+    public function deleteNote(string $id): string
     {
         $id = Operations::decryptId($id);
-        
+
         return "I´m deleting note with id = $id";
     }
-
-    
 }
